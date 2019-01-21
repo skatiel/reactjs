d96f05debaf4d5895a770e3d36a8f73119f0ee1f
Serge <skatie@mail.ru>
2019-01-22 02:17:19 +0300

    Написана функция loop

---
+++ loop.js
@@ -0,0 +1,13 @@
+function loop (times = 0, callback = null) {
+  if (callback != null) {
+    for (let i = 0; i < times; i++) {
+      callback ('hello');
+    }
+  }
+}
+
+function callback (name) {
+    console.log(name);
+}
+
+loop(10, callback);
